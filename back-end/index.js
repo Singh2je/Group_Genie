6538fbc68c04b447d37c3891efddba0297f6444e
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2024-02-24 17:23:15 -0500

    Initial commit for react and node.js

---
+++ back-end/index.js
@@ -0,0 +1,22 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
