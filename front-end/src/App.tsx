8e5b43c0b933d33a03e0ecbb4130033b34a2add6
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2024-02-24 20:52:32 -0500

    removed comments

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ClusterForm from '../../front-end/src/Components/ClusterForm'; // Adjust the path according to your file structure
+import ClusterForm from '../../front-end/src/Components/ClusterForm'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Other components or content can also go here */}
-        <ClusterForm /> {/* This line adds the ClusterForm to your UI */}
+        <ClusterForm /> 
       </header>
     </div>
   );
